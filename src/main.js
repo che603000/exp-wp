38a68f0c1beac2bd1978389bd2b554bc7150f28e
Home <che603000@gmail.com>
2016-08-04 13:29:59 +0300

    web pack config

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 /**
  * Created by Администратор on 16.07.2016.
  */
-import './index.css'
-import  './libs/chart'
+//import './index.css'
+//import  './libs/chart'
+
+const $ = require('jquery');
+//require('bootstrap-css/js/bootstrap.js');
+
+console.log($);
--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,31 @@
  */
 const path = require('path'),
     webpack = require('webpack'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
     BowerWebpackPlugin = require("bower-webpack-plugin");
 
 
 module.exports = {
-    entry: [
-        //'babel-polyfill',
-        //'./src/theme/main.less',
-        './src/main',
-        'webpack-dev-server/client?http://localhost:8080'
-    ],
-    externals:{
-        "jquery": "jQuery"
+    entry: {
+        app: './src/main',
+        vendors: ['jquery', 'backbone','bootstrap-css'],
+        "dev-server": 'webpack-dev-server/client?http://localhost:8080'
     },
     output: {
-        publicPath: '/',
+        publicPath: './',
         path: './public',
-        filename: 'main.js'
+        filename: '[name].js'
     },
-    debug: true,
-    devtool: 'source-map',
+
+    externals: {
+        //"jquery": "jQuery"
+    },
+    resolve: {
+        modulesDirectories: ['node_modules', 'bower_components'],
+        alias:{
+
+        }
+    },
     module: {
         loaders: [
             {
@@ -30,7 +35,6 @@ module.exports = {
                 include: path.join(__dirname, 'src'),
                 exclude: /node_modules|bower_components/,
                 loader: 'babel-loader',
-
                 query: {
                     plugins: ['transform-runtime', 'transform-decorators-legacy'],
                     presets: ['es2015', 'stage-0']
@@ -41,14 +45,38 @@ module.exports = {
                 loader: "style!css!autoprefixer!less"
             },
             {test: /\.css$/, loader: "style!css"},
-
+            { test: /\.(woff2?|svg)$/, loader: 'url?limit=10000' },
+            { test: /\.(ttf|eot)$/, loader: 'file' },
+            { test: /bootstrap-css.js/, loader: 'imports?jQuery=jquery' },
         ],
     },
+
     plugins: [
         new webpack.OldWatchingPlugin(),
         //new BowerWebpackPlugin()
+        new BowerWebpackPlugin({
+            modulesDirectories: ["bower_components"],
+            manifestFiles: ".bower.json",
+            includes: /.*/,
+            excludes: [],
+            searchResolveModulesDirectories: true
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "vendors",
+            //filename: "index.js",
+            minChunks: Infinity,
+        }),
+        new HtmlWebpackPlugin({
+            title: 'My App',
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+
     ],
+
     devServer: {
         contentBase: "./public"
-    }
+    },
+    //debug: true,
+   // devtool: 'source-map',
 };
